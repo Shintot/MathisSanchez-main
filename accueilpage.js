f4fcd3d20dfbd61bce4811d419ec90ab276dc049
Mathis <rugby2116@gmail.com>
2022-06-25 13:34:42 +0200

    Add
    - Bloque le Js pour responsive

--- accueilpage.js
+++ accueilpage.js
@@ -1,62 +1,75 @@
-// SLIDER BOUTON
-const slidingNewsletter = document.querySelector(".transition");
-window.addEventListener("scroll", () => {
-  const { scrollTop, clientHeight } = document.documentElement;
-  const topElementToTopViewport = slidingNewsletter.getBoundingClientRect().top;
+$(function() {
 
-  if (
-    scrollTop >
-    (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.8
-  ) {
-    slidingNewsletter.classList.add("active");
+  if ($(window).width()>480){
+    // SLIDER BOUTON
+    const slidingNewsletter = document.querySelector(".transition");
+    window.addEventListener("scroll", () => {
+      const { scrollTop, clientHeight } = document.documentElement;
+      const topElementToTopViewport =
+        slidingNewsletter.getBoundingClientRect().top;
+
+      if (
+        scrollTop >
+        (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.8
+      ) {
+        slidingNewsletter.classList.add("active");
+      }
+    });
+
+    // SLIDER PRESENTATION
+    const slidingpresentation = document.querySelector(".transition2");
+    window.addEventListener("scroll", () => {
+      const { scrollTop, clientHeight } = document.documentElement;
+      const topElementToTopViewport =
+        slidingpresentation.getBoundingClientRect().top;
+
+      console.log(topElementToTopViewport);
+
+      if (
+        scrollTop >
+        (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.8
+      ) {
+        slidingpresentation.classList.add("active");
+      }
+    });
+
+    // SLIDER MAQUETTE
+    const slidingmaquette = document.querySelector(".template");
+    window.addEventListener("scroll", () => {
+      const { scrollTop, clientHeight } = document.documentElement;
+      const topElementToTopViewport =
+        slidingmaquette.getBoundingClientRect().top;
+
+      console.log(topElementToTopViewport);
+
+      if (
+        scrollTop >
+        (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.8
+      ) {
+        slidingmaquette.classList.add("active");
+      }
+    });
+
+    // SLIDER dessin
+    const slidindessin = document.querySelector(".dessinmoi");
+    window.addEventListener("scroll", () => {
+      const { scrollTop, clientHeight } = document.documentElement;
+      const topElementToTopViewport = slidindessin.getBoundingClientRect().top;
+
+      console.log(topElementToTopViewport);
+
+      if (
+        scrollTop >
+        (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.8
+      ) {
+        slidindessin.classList.add("active");
+      }
+    });
   }
+
+ else{}
+
 });
 
-// SLIDER PRESENTATION
-const slidingpresentation = document.querySelector(".transition2");
-window.addEventListener("scroll", () => {
-  const { scrollTop, clientHeight } = document.documentElement;
-  const topElementToTopViewport = slidingpresentation.getBoundingClientRect().top;
 
-  console.log(topElementToTopViewport);
-
-  if (
-    scrollTop >
-    (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.8
-  ) {
-    slidingpresentation.classList.add("active");
-  }
-});
-
-// SLIDER MAQUETTE
-const slidingmaquette = document.querySelector(".template");
-window.addEventListener("scroll", () => {
-  const { scrollTop, clientHeight } = document.documentElement;
-  const topElementToTopViewport = slidingmaquette.getBoundingClientRect().top;
-
-  console.log(topElementToTopViewport);
-
-  if (
-    scrollTop >
-    (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.8
-  ) {
-    slidingmaquette.classList.add("active");
-  }
-});
-
-// SLIDER dessin
-const slidindessin = document.querySelector(".dessinmoi");
-window.addEventListener("scroll", () => {
-  const { scrollTop, clientHeight } = document.documentElement;
-  const topElementToTopViewport = slidindessin.getBoundingClientRect().top;
-
-  console.log(topElementToTopViewport);
-
-  if (
-    scrollTop >
-    (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.8
-  ) {
-    slidindessin.classList.add("active");
-  }
-});
 
